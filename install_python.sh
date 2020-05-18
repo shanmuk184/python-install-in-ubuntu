9cedb11447bc94723cec8341f211f099dd940b77
Shanmuk <shanmuksastry@gmail.com>
2020-05-18 17:25:25 +0530

    Update install_python.sh

--- install_python.sh
+++ install_python.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install python3
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 sudo apt install python3-pip
+sudo apt update
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
 python ––version
 
